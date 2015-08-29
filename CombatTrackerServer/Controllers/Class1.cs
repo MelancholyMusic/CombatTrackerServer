c9dc446645e59796660d1af7374c8bd318f5a95e
James <bloom.james7@gmail.com>
2015-08-29 16:13:45 -0600

    Entity & Web API

---
+++ CombatTrackerServer/Controllers/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web;
+using System.Web.Http;
+
+namespace CombatTrackerServer.Controllers
+{
+    public class Class1 : ApiController
+    {
+        [HttpGet]
+        [Route("activities")]
+        public HttpResponseMessage GetActivity()
+        {
+            // Event doesn't exist, can't add activity.
+            return new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK
+            };
+        }
+    }
+}
--- CombatTrackerServer/Controllers/Site/ManageController.cs
+++ CombatTrackerServer/Controllers/Site/ManageController.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using CombatTrackerServer.Models.Site;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
-using CombatTrackerServer.Models;
 
-namespace CombatTrackerServer.Controllers
+namespace CombatTrackerServer.Controllers.Site
 {
     [Authorize]
     public class ManageController : Controller
